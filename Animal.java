3c886ff1c3cd5b4c4d8457af64b57083a61bd970
shobri0101 <muhammadshobri448@gmail.com>
2025-03-03 21:23:08 +0700

    class Animal
    class Animal extent

---
+++ Animal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minggu5wsibd;
+
+/**
+ *
+ * @author Muhammad Shobri
+ */
+class Animal {
+    public void makeNoise() {
+        System.out.println("talk");
+    }
+}
+
