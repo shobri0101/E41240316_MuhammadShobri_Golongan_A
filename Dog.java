9460d28ada93a22d194a0f5b16276981060d8428
shobri0101 <muhammadshobri448@gmail.com>
2025-03-03 21:26:42 +0700

    soal no 3
    class Dog

---
+++ Dog.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minggu5wsibd;
+
+public class Dog extends Animal {
+    @Override
+    public void makeNoise() {
+        System.out.println("Bark");
+    }
+}
+
